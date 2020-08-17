09b26223404c01ebad85cb58f7c0aab42af25a42
xiaolun <3493826518@qq.com>
2020-08-17 17:47:28 +0800

    spring cloud code upload 01

--- cloud-consumerzk-order80/src/main/java/com/xiaolun/springcloud/OrderZKMain80.java
+++ cloud-consumerzk-order80/src/main/java/com/xiaolun/springcloud/OrderZKMain80.java
@@ -4,15 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @EnableDiscoveryClient
- *  此时不用添加Eureka
- *  该注解用于向使用consul或者zookeeper作为注册中心时注册服务
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentMain8004 {
+public class OrderZKMain80 {
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentMain8004.class, args);
+		SpringApplication.run(OrderZKMain80.class,args);
 	}
 }
--- cloud-providerconsul-payment8006/src/main/java/com/xiaolun/springcloud/PaymentMain8006.java
+++ cloud-providerconsul-payment8006/src/main/java/com/xiaolun/springcloud/PaymentMain8006.java
@@ -1,18 +1,14 @@
 package com.xiaolun.springcloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @EnableDiscoveryClient
- *  此时不用添加Eureka
- *  该注解用于向使用consul或者zookeeper作为注册中心时注册服务
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentMain8004 {
+public class PaymentMain8006 {
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentMain8004.class, args);
+		SpringApplication.run(PaymentMain8006.class,args);
 	}
 }
